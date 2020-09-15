a7673df5ab0d6827a9d01cb299aa5d555e77a834
javiemgz <javiemgz@gmail.com>
2020-09-14 23:42:02 -0300

    refactores api response

--- src/services/trasactionsService.ts
+++ src/services/trasactionsService.ts
@@ -4,23 +4,18 @@ import transactionRepo from "../repository/transactionRepo";
 class TransactiosService {
   async getReport(filters: Array<Object>) {
     try {
-      const expenses = await transactionRepo.categoriesReport(
-        filters,
-        true
+      const expenses = await transactionRepo.categoriesReport(filters, true);
+      const earnings = await transactionRepo.categoriesReport(filters, false);
+      const total = await transactionRepo.totalExpensesReport(filters);
+      const balanceStatus = total.reduce(
+        (acum, rep) => acum - rep.value,
+        total[0].value * 2
       );
-      const earnings = await transactionRepo.categoriesReport(
-        filters,
-        false
-      );
-      const total = await transactionRepo.totalExpensesReport(
-        filters,
-      );
-      const balanceStatus = total[0].value - total[1].value
-      total.push({finalBalance: balanceStatus})
       const report = {
         expenses,
         earnings,
         total,
+        balanceStatus,
       };
       return Promise.resolve(report);
     } catch (e) {
